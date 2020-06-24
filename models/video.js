659881bb8635d941930d0e03986871ae8f49304c
Dania King <dania@fortunebuilders.com>
2020-06-23 22:38:10 -0700

    Working API, updated Models by adding User, Updates Seed file to match google API return

--- models/video.js
+++ models/video.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const videoSchema = new Schema({
   title: { type: String, required: true },
-  // thumbnail: {type: String, required: true},
-  // id: {type: String, required: true},
-  author: { type: String, required: true },
-  url: {type: String, required: true},
+  thumbnail: {type: String, required: true},
+  videoId: {type: String, required: true},
+  videoUrl: {type: String, required: true},
+  description: {type: String, required: true},
   date: { type: Date, default: Date.now }
 });
 
