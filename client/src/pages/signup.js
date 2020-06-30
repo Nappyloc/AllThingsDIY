a3849fc7736b917e3d18bf746e2ff0f9fc3668fd
Hung Le <hungle913@gmail.com>
2020-06-29 20:58:51 -0700

    added function to make sign-up botton work and save user to DB

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,65 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
+import API from "../utils/API";
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBInput } from 'mdbreact';
 
-const FormPage = () => {
-  return (
-    
-    <MDBContainer>
-      <MDBRow>
+class FormPage extends Component {
+
+  state = {
+    userName: "",
+    password: ""
+  };
+
+  inputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  createUsername = event => {
+    event.preventDefault();
+    if (this.state.userName && this.state.password) {
+      API.createUser({
+        userName: this.state.userName,
+        password: this.state.password
+      })
+        .then(res => this.setState({ userName: "", password: "" }))
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+
+    return (
+      <MDBContainer>
+        <MDBRow>
           <MDBCard
-            className='card-image'
-            className="center"
+            className='card-image center'
             style={{
               backgroundColor:
-               'default-color-dark',
+                'default-color-dark',
               width: '28rem'
             }}
           >
-            <div className='text-dark rgba-stylish-strong py-5 px-5 z-depth-4'>
-              <div className='text-center'>
-                <h3 className='white-text mb-5 mt-4 font-weight-bold'>
-                <h3 className='green-text font-weight-bold'>
-                  <strong>SIGN UP</strong>
-                  </h3>
-                </h3>
-              </div>
-              <MDBInput
-                label='New username'
-                group
-                type='text'
-                validate
-                labelClass='white-text'
-              />
-              <MDBInput
-                label='New password'
-                group
-                type='password'
-                validate
-                labelClass='white-text'
-              />
-              <MDBInput
-                label='Repeat password'
-                group
-                type='password'
-                validate
-                labelClass='white-text'
-              />
-              <div className='md-form pb-3'>
-                <MDBInput
-                  label={
-                    <>
-                      Accept the&nbsp;
-                      <a href='#!' className='green-text font-weight-bold'>
-                        Terms and Conditions
-                      </a>
-                    </>
-                  }
-                  type='checkbox'
-                  id='checkbox1'
-                  labelClass='white-text'
-                />
-              </div>
-              <MDBCol>
+          <div className='text-dark rgba-stylish-strong py-5 px-5 z-depth-4'>
+            <div className='text-center'>
+              <h3 className='white-text mb-5 mt-4 font-weight-bold'>
+                <strong>SIGN UP</strong>
+              </h3>
+            </div>
+            <MDBInput
+              value={this.state.userName}
+              onChange={this.inputChange}
+              name="userName"
+              label='New username'
+              group
+              type='text'
+              validate
+              labelClass='white-text'
+            />
+            <MDBInput
+            value={this.state.password}
+            onChange={this.inputChange}
+            name="password"
+              label='New password'
+              group
+              type='password'
+              validate
+              labelClass='white-text'
+            />
+            <MDBCol>
               <MDBRow className='d-flex align-items-center mb-4'>
                 <div className='text-center mb-3 col-md-12'>
                   <MDBBtn
@@ -67,25 +75,26 @@ const FormPage = () => {
                     rounded
                     type='button'
                     className='btn-block z-depth-1'
-                  >
-                    Sign in
+                    onClick={this.createUsername}
+                  >Sign in
                   </MDBBtn>
                 </div>
               </MDBRow>
               <MDBCol md='12'>
                 <p className='font-small white-text d-flex justify-content-end'>
                   Have an account?
-                  <a href='#!' className='green-text ml-1 font-weight-bold'>
+                  <a href='/login' className='green-text ml-1 font-weight-bold'>
                     Log in
                   </a>
                 </p>
-                </MDBCol>
-                </MDBCol>
-            </div>
+              </MDBCol>
+            </MDBCol>
+          </div>
           </MDBCard>
-      </MDBRow>
-    </MDBContainer>
-  );
-};
+        </MDBRow>
+      </MDBContainer>
+    );
+  };
+}
 
 export default FormPage;
