51ac607fcdb428a704073f0abd391985990f11ff
Darko Jovevski <platzmanfan9@gmail.com>
2020-06-29 19:43:39 -0700

    added landing page and profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
-import Signup from "./components/signup";
-import Landing from "./components/landing"
+import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
+// import Signup from "./components/signup";
+import Landing from "./pages/landing"
+import Signup from "./pages/signup"
+import Profile from "./pages/profile"
+
+// import Jumbotron from "./components/Jumbotron";
+// import Profile from "./components/profile";
 
 function App() {
   return (
-  <Landing/>
+    <Router>
+    <div>
+
+      <Landing/>
+      {/* <Switch>
+      <Route exact path="/" component={Landing}/>
+      <Route path="/signup" component={Signup}/>
+      <Route path="/profile" component={Profile}/>
+      </Switch> */}
+  
+  </div>
+  </Router>
   )
 
 }
---
+++ client/src/pages/profile.js
@@ -0,0 +1,187 @@
+import React from "react";
+import { FiTwitter } from 'react-icons/fa';
+import { BrowserRouter } from 'react-router-dom';
+import { MDBNav, MDBNavItem, MDBNavLink, MDBCol,MDBRow ,
+MDBIcon,MDBJumbotron,MDBCardImage,MDBCardTitle,MDBCardBody,MDBCardText,MDBContainer} from "mdbreact";
+import "./profile.css"
+
+export default () => (
+  <BrowserRouter>
+    <MDBRow>
+        <MDBCol>
+        <MDBNav className="justify-content-center">
+      <MDBNavItem>
+        <MDBNavLink active to="/">Home</MDBNavLink>
+      </MDBNavItem>
+      <MDBNavItem>
+        <MDBNavLink to="#!">Link</MDBNavLink>
+      </MDBNavItem>
+      <MDBNavItem>
+        <MDBNavLink to="#!">Link</MDBNavLink>
+      </MDBNavItem>
+    
+    </MDBNav>
+        </MDBCol>
+    </MDBRow>
+    <MDBRow>
+        <MDBCol md="4">
+
+    <MDBContainer className="mt-5 text-center">
+      <MDBRow>
+        <MDBCol>
+          <MDBJumbotron className="text-center">
+            <MDBCardTitle className="card-title h4 pb-2">
+              <strong>My saved projects</strong>
+            </MDBCardTitle>
+
+            <MDBCardImage
+              src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+              className="img-fluid"
+            />
+            <MDBCardBody>
+              <MDBCardTitle className="indigo-text h5 m-4">
+                Link
+              </MDBCardTitle>
+              <MDBCardText>
+               Description
+              </MDBCardText>
+
+              <MDBCol className="d-flex justify-content-center mt-4" md="12">
+                <MDBCol md="3" className="d-flex justify-content-around">
+                  <a href="#"><MDBIcon
+                    fab
+                    icon="linkedin-in"
+                    className="grey-text"
+                    size="lg"
+                  /></a>
+                  <a href="#"><MDBIcon
+                    fab
+                    icon="twitter"
+                    className="grey-text"
+                    size="lg"
+                  /></a>
+                  <a href="#"><MDBIcon
+                    fab
+                    icon="facebook-f"
+                    className="grey-text"
+                    size="lg"
+                  /></a>
+                </MDBCol>
+              </MDBCol>
+
+            </MDBCardBody>
+          </MDBJumbotron>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  
+        </MDBCol>
+        <MDBCol md="4">
+
+<MDBContainer className="mt-5 text-center">
+  <MDBRow>
+    <MDBCol>
+      <MDBJumbotron className="text-center">
+        <MDBCardTitle className="card-title h4 pb-2">
+          <strong>My saved projects</strong>
+        </MDBCardTitle>
+
+        <MDBCardImage
+          src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+          className="img-fluid"
+        />
+        <MDBCardBody>
+          <MDBCardTitle className="indigo-text h5 m-4">
+            Link
+          </MDBCardTitle>
+          <MDBCardText>
+           Description
+          </MDBCardText>
+
+          <MDBCol className="d-flex justify-content-center mt-4" md="12">
+            <MDBCol md="3" className="d-flex justify-content-around">
+              <a href="#"><MDBIcon
+                fab
+                icon="linkedin-in"
+                className="grey-text"
+                size="lg"
+              /></a>
+              <a href="#"><MDBIcon
+                fab
+                icon="twitter"
+                className="grey-text"
+                size="lg"
+              /></a>
+              <a href="#"><MDBIcon
+                fab
+                icon="facebook-f"
+                className="grey-text"
+                size="lg"
+              /></a>
+            </MDBCol>
+          </MDBCol>
+
+        </MDBCardBody>
+      </MDBJumbotron>
+    </MDBCol>
+  </MDBRow>
+</MDBContainer>
+
+
+    </MDBCol>
+    <MDBCol md="4">
+
+<MDBContainer className="mt-5 text-center">
+  <MDBRow>
+    <MDBCol>
+      <MDBJumbotron className="text-center">
+        <MDBCardTitle className="card-title h4 pb-2">
+          <strong>My saved projects</strong>
+        </MDBCardTitle>
+
+        <MDBCardImage
+          src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+          className="img-fluid"
+        />
+        <MDBCardBody>
+          <MDBCardTitle className="indigo-text h5 m-4">
+            Link
+          </MDBCardTitle>
+          <MDBCardText>
+           Description
+         
+          </MDBCardText>
+
+          <MDBCol className="d-flex justify-content-center mt-4" md="12">
+            <MDBCol md="3" className="d-flex justify-content-around ">
+              <a href="#"><MDBIcon
+                fab
+                icon="linkedin-in"
+                className="grey-text"
+                size="lg"
+              /></a>
+              <a href="#">
+                  
+                  </a>
+              <a href="#"><MDBIcon
+                fab
+                icon="facebook-f"
+                className="grey-text"
+                size="lg"
+              /></a>
+            </MDBCol>
+          </MDBCol>
+
+        </MDBCardBody>
+      </MDBJumbotron>
+    </MDBCol>
+  </MDBRow>
+</MDBContainer>
+
+
+    </MDBCol>
+    </MDBRow>
+   
+
+  </BrowserRouter>
+);
