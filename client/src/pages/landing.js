dbc11e2efc34b1b7902f243a5838f5b808568ad0
Dania King <dania@fortunebuilders.com>
2020-06-29 22:26:45 -0700

    updated search function

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,7 +1,8 @@
 import React from "react";
 // import { FaBeer } from 'react-icons/fa';
 
-import {
+import
+{
   MDBNavbar,
   MDBNavbarBrand,
   MDBNavbarNav,
@@ -22,28 +23,103 @@ import {
   MDBFormInline,
   MDBAnimation,
   MDBDropdown,
-  MDBDropdownToggle, 
-  MDBDropdownMenu, 
+  MDBDropdownToggle,
+  MDBDropdownMenu,
   MDBDropdownItem,
   MDBLink
 } from "mdbreact";
 import "./landing.css";
+import API from "../utils/API";
 
 
-class ClassicFormPage extends React.Component {
+class ClassicFormPage extends React.Component
+{
   state = {
-    collapseID: ""
+    collapseID: "",
+    search: ""
   };
 
   toggleCollapse = collapseID => () =>
-    this.setState(prevState => ({
+    this.setState( prevState => ( {
       collapseID: prevState.collapseID !== collapseID ? collapseID : ""
-    }));
+    } ) );
 
-    
-  render() {
+  inputChange = event =>
+  {
+    const { name, value } = event.target;
+    this.setState( {
+      [ name ]: value
+    } );
+  };
+
+
+
+  diySearch = event =>
+  {
+    event.preventDefault()
+    API.topicSearch( this.state.search )
+      .then( res => this.setState( { search: "" } ) )
+      .catch( err => console.log( err ) );
+  }
+
+
+  render ()
+  {
+    const overlay = (
+      <div
+        id="sidenav-overlay"
+        style={{ backgroundColor: "transparent" }}
+        onClick={this.toggleCollapse( "navbarCollapse" )}
+      />
+    );
     return (
       <div id="classicformpage">
+        <div>
+          <MDBNavbar dark expand="lg" fixed="top">
+            <MDBContainer>
+              <MDBNavbarToggler
+                onClick={this.toggleCollapse( "navbarCollapse" )}
+              />
+              <MDBCollapse
+                id="navbarCollapse"
+                isOpen={this.state.collapseID}
+                navbar
+              >
+                <MDBDropdown>
+                  <MDBDropdownToggle caret color="primary">
+                    Dropdown
+        </MDBDropdownToggle>
+                  <MDBDropdownMenu basic >
+                    <MDBDropdownItem>Action</MDBDropdownItem>
+                    <MDBDropdownItem>Another Action</MDBDropdownItem>
+                    <MDBDropdownItem>Something else here</MDBDropdownItem>
+                    <MDBDropdownItem divider />
+                    <MDBDropdownItem>Separated link</MDBDropdownItem>
+                  </MDBDropdownMenu>
+                </MDBDropdown>
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to="/">Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="/profile">Profile</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+                <MDBNavbarNav right>
+                  <MDBNavItem active>
+                    <MDBNavLink to="/login">Login</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem active>
+                    <MDBNavLink to="/signup"> SignUp</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBContainer>
+          </MDBNavbar>
+
+          {this.state.collapseID && overlay}
+        </div>
+
         <MDBView>
           <MDBMask className="d-flex justify-content-center align-items-center gradient">
             <MDBContainer>
@@ -54,29 +130,46 @@ class ClassicFormPage extends React.Component {
                   className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5"
                 >
                   <hr className="hr-light" />
-                </MDBAnimation>       
+                </MDBAnimation>
               </MDBRow>
               <MDBRow>
-            <MDBCol>
-                        <div className="md-form my-0">
-                          <input
-                            className="form-control mr-sm-2"
-                            type="text"
-                            placeholder="Search for project you want to build"
-                            aria-label="Search"
-                            width="350px"
-                            height="250px"
-                          />
-                        </div>
-            </MDBCol>
-        </MDBRow>
+                <MDBCol>
+                  <div className="md-form my-0">
+                    <input
+                      className="form-control mr-sm-2"
+                      type="text"
+                      value={this.state.search}
+                      onChange={this.inputChange}
+                      name="search"
+                      placeholder="Search for project you want to build"
+                      aria-label="Search"
+                      width="350px"
+                      height="250px"
+                      
+                     
+
+                    />
+                    <MDBBtn
+                      color='success'
+                      rounded
+                      type='button'
+                      className=""
+                      onClick={this.diySearch}
+                    >
+                      Search
+                </MDBBtn>
+
+                  </div>
+                </MDBCol>
+              </MDBRow>
             </MDBContainer>
           </MDBMask>
         </MDBView>
-        <MDBContainer>  
+        <MDBContainer>
+
           <MDBRow className="py-5">
             <MDBCol md="12" className="text-center">
-              
+
             </MDBCol>
           </MDBRow>
         </MDBContainer>
